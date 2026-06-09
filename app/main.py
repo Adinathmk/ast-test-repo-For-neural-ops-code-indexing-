6632c678777463341f3d16a53880abd4a43e4d66
Adinath M K <168068778+Adinathmk@users.noreply.github.com>
2026-06-09 15:14:53 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,5 +5,5 @@ class LogProcessor:
     def __init__(self, endpoint: str):
         self.endpoint = endpoint
     def process_incoming_stream(self, data: list):
-        calculate_metrics(1,2)
+        calculate_metrics(1,3)
         print(f"Processing {len(data)} items...")
